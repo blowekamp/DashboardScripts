e90abf37a4f60ec12d6ea725f56260755600e18c
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-01-03 21:10:59 -0500

    adding hook to upload on release builds

---
+++ UploadHook.cmake
@@ -0,0 +1,16 @@
+
+macro ( dashboard_hook_end )
+  message( "build_number_errors: ${build_number_errors}" )
+  if ( ${build_number_errors} EQUAL "0" )
+    message ( "dashboard_git_branch: ${dashboard_git_branch}" )
+    file ( GLOB PY_EGG "${CTEST_DASHBOARD_ROOT}/SimpleITK-build/SimpleITK-build/Wrapping/dist/*.egg" )
+    set ( packages ${PY_EGG} )
+    foreach( f ${packages} )
+      message( "UPLOADING: ${f}" )
+      execute_process ( COMMAND scp "${f}" "blowek1@erie.nlm.nih.gov:/Users/blowek1/Sites/SimpleITKPackages" )
+    endforeach()
+
+  else()
+    message( "Build failure, not uploading!" )
+  endif()
+endmacro()
